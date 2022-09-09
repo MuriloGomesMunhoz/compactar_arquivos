ec7d6888a855937cfe77a7a32f2d44587d36d7c0
MuriloGomesMunhoz <murilogmunhoz@hotmail.com>
2022-09-09 15:17:08 -0300

    Adicionado função descompactar

--- compactar.sh
+++ compactar.sh
@@ -4,18 +4,39 @@ VARTAR="tar"
 
 case "$1" in
 	"command")
-		echo "1°: extensão"
-		echo "2°: arquivo que deseja compactar"
-		echo "3°: como deve ser o nome do arquivo compactado"
+		echo "1°: compactar(c) | descompactar(d)"
+		echo "2°: extensão"
+		echo "3°: arquivo que deseja compactar"
+		echo "4°: como deve ser o nome do arquivo compactado"
 		;;
-	"tar")
-		tar -czf "$3".tar.gz "$2"
-		echo "compactado para tar $2"
-                ;;
-	"zip")
-		zip "$3".zip "$2" 
-		echo "compactado para zip $2"
+
+	"c")
+		case "$2" in
+			"tar")
+				tar -czf "$3".tar.gz "$3"
+				echo "compactado para tar $3"
+                		;;
+			"zip")
+				zip "$4".zip "$3" 
+				echo "compactado para zip $3"
+				;;
+			esac
 		;;
+
+
+	"d")
+		case "$2" in
+			"tar")
+				tar -vzxf "$3".tar.g
+				echo "descompactado para tar $3"
+                		;;
+			"zip")
+				unzip "$3".zip 
+				echo "descompactado para zip $3"
+				;;
+			esac
+		;;
+
+
+
 esac
-
-
